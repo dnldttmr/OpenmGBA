c9b99333207ba6cfc81786c471c44e0ef19a8921
Daniel Dettmer <daniel.dettmer@adesso.de>
2026-08-04 10:04:10 +0200

    Sync browser history with active ROM so back navigation returns to the library

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { LibraryScreen } from './screens/LibraryScreen'
 import { PlayerScreen } from './screens/PlayerScreen'
 import { useLibraryStore } from './store/libraryStore'
+import { useHistorySync } from './hooks/useHistorySync'
 
 function App() {
   const activeRomId = useLibraryStore((state) => state.activeRomId)
+  useHistorySync()
   return activeRomId ? <PlayerScreen /> : <LibraryScreen />
 }
 
